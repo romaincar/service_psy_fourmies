e295457ddbbf813dcbbdc12acd39575cf2e44cd3
rom <c59.romain@live.fr>
2019-03-19 09:35:50 +0100

    projet visite psy

---
+++ visite.js
@@ -0,0 +1,251 @@
+visite=[
+    {
+        principal: "image_visite/01.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entrée"
+    },
+
+    {
+        principal: "image_visite/02.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entrée"
+    },
+
+    {
+        principal: "image_visite/02.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+
+    {
+        principal: "image_visite/04.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+
+    {
+        principal: "image_visite/06.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/08.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/07.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/08.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/09.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/10.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/06.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/11.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/12.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/13.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/14.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/12.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/15.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/04.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/20.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/04.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/21.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/22.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/21.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/23.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/21.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/24.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/25.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/26.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/27.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/30.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/27.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/28.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/29.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/26.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/31.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/32.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/34.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/33.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    },
+    {
+        principal: "image_visite/35.JPG",
+        titre: "Unité pour Anxio-Dépressifs",
+        commentaire: "Entréé"
+    }
+
+];
+var i =0;
+console.log(visite[i].principal);
+
+
+for ( i = 0 ; i < visite.length -1; i++){
+
+    if (i = 0){
+        document.write =
+        '<div class="item active">' +
+        '<img src="'+ visite[i].principal +'" alt="Chicago" style="width:100%;">' +
+        '<div class="carousel-caption">' +
+        '<h3>' + visite[i].titre + '</h3>' +
+        '<p>' + visite[i].commentaire + '</p>' +
+        '</div>' +
+        '</div>';
+    }
+    else {
+        document.write =
+        '<div class="item">' +
+        '<img src="'+ visite[i].principal +'" alt="Chicago" style="width:100%;">' +
+        '<div class="carousel-caption">' +
+        '<h3>' + visite[i].titre + '</h3>' +
+        '<p>' + visite[i].commentaire + '</p>' +
+        '</div>' +
+        '</div>';
+    }
+
+}
+
+
+/*var div1 = document.createElement('div');
+div1.class = "item";
+var img = document.createElement('img');
+img.source = visite[i].principal;
+img.style = "width";
+var div2 = document.createElement('div');
+div2.class = "carousel-caption";
+var h3 = document.createElement('h3');
+
+var p = document.createElement( 'p');
+
+div1.appendChild('photo');
+img.appendChild('photo');
+div2.appendChild('photo');
+*/
+
+
+h3.innerHTML = visite[i].titre;
+p.innerHTML = visite[i].commentaire;
